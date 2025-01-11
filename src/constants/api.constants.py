d2e25a1e34454d32c7676af9601819bb4e8109dc
Abhijeet <abhijeetkakpure1190@gmail.com>
2025-01-11 22:20:40 +0530

    Updated

---
+++ src/constants/api.constants.py
@@ -0,0 +1,14 @@
+# APIConstants - Class which contains all endpoints
+#Keep the URLs
+
+
+class APIConstants:
+
+     def base_url(self):
+         return "https://restful-booker.herokuapp.com/booking"
+
+     def create_token(self):
+         return "https://restful-booker.herokuapp.com/auth"
+
+     def Get_bookingID(self):
+         return "https://restful-booker.herokuapp.com/booking"
--- src/tests/sample_testcase2.py
+++ src/tests/sample_testcase2.py
@@ -4,3 +4,8 @@ import allure
 @allure.title("Sample Testcase")
 def test_sample():
         assert True == True
+
+
+@allure.title("Sample Testcase2")
+def test_sample2():
+        assert True == True
